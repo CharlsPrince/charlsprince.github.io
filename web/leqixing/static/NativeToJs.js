6d1bab89b1a2809da6d5d0b96cd78f58ecb4ef1a
Charls <961629701@qq.com>
2018-04-19 00:38:00 +0800

    Site updated: 2018-04-19 00:37:59

--- web/leqixing/static/NativeToJs.js
+++
@@ -1,7 +0,0 @@
-
-function javacalljswith(arg){
-  var obj = JSON.parse(arg);
-  sessionStorage.userId = obj.userId;
-  sessionStorage.token = obj.token;
-  document.getElementById("my-card-token").value = (sessionStorage.token);
-}
